db1257259d7ec6cf51be3e0c732f87e54a60e232
Ozziekins <o.okonicha@innopolis.university>
2025-04-03 17:39:29 +0200

    changed A to button in solid for uniform e2e test

--- quizz-app-solid/src/components/Hero.tsx
+++ quizz-app-solid/src/components/Hero.tsx
@@ -1,12 +1,16 @@
-import { A } from "@solidjs/router";
+import { useNavigate } from "@solidjs/router";
 
 export default function Hero() {
+	const navigate = useNavigate();
+
 	return (
 		<section class="hero">
 			<h1 class="hero-title">a remarkably fun quizz app</h1>
 			<p class="hero-desc">make quizzes and take quizzes with others.</p>
 			<div class="hero-actions">
-				<A href="/quiz?random=true">take quiz</A>
+				<button type="button" onClick={() => navigate("/quiz?random=true")}>
+					take quiz
+				</button>
 			</div>
 		</section>
 	);
