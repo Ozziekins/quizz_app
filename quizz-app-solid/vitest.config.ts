393efd22a1989b1863863da4a727325c85da8869
Ozziekins <o.okonicha@innopolis.university>
2025-04-03 17:05:36 +0200

    added 3001 port for solid

--- quizz-app-solid/vitest.config.ts
+++ quizz-app-solid/vitest.config.ts
@@ -3,6 +3,9 @@ import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
 	plugins: [solidPlugin()],
+	preview: {
+		port: 3001,
+	},
 	test: {
 		globals: true,
 		environment: "jsdom",
